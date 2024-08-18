c143cee9680fa17e358230f5ded0feb7706895d3
Greison Montenari <greisonmontenari02@gmail.com>
2024-08-18 17:53:24 -0300

    teste

---
+++ aula3.py
@@ -0,0 +1,7 @@
+print('Greison Montenari')
+
+print("Greison Montenari")
+
+print(r"Greison Montenari")
+
+print(1, 'Greison "Montenari"')
---
+++ aula6.py
@@ -0,0 +1,11 @@
+nome = 'Greison Montenari'
+altura = 1.69
+peso = 82
+imc = peso / altura ** 2
+print(nome, 'tem', altura, 'de altura',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
+# Greison Montenari tem 1.69 de altura,
+# pesa 82 quilos e seu IMC é 
+#28.71047932495361
